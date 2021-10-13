466f0dff4de26f29c2f323180629b398e5a1797d
Cyn <cynbertea0764@gmail.com>
2021-10-13 15:08:54 -0300

    agrego formulario de contacto

---
+++ js/formulario.js
@@ -0,0 +1,9 @@
+let datosform = function() {
+    let nombre = document.getElementById ("nombre").value; 
+    let apellido = document.getElementById ("apellido").value;
+    let correo = document.getElementById ("correoelectronico").value;
+    let consulta = document.getElementById ("consulta").value;
+    console.log (nombre + " " +apellido + " " +correo + " " +consulta);
+
+}
+
