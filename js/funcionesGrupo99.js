3058311221cb5e7e93071ab0340a7ed8fba5946d
ramiro <ramiro.g.g.1993@gmail.com>
2021-11-13 17:19:12 -0300

    Se movieron las funciones al archivo funcionesGrupo99.js y ademas se aregaron comentarios en cada punto realizado

--- js/funcionesGrupo99.js
+++ js/funcionesGrupo99.js
@@ -21,4 +21,42 @@ submitBtn.addEventListener("click", function () {
    
 
 })
+//funcion validar mas de 2 caracteres (en el login.html boton enviar de contacto)
+function checkLength(){
+    var textbox = document.getElementById("exampleInputName");
+    var textbox2 = document.getElementById("exampleInputSurname");
+    if((textbox.value.length && textbox2.value.length)>=2 && (textbox.value.length && textbox2.value.length) <= 20){
+        return true;
+    }
+    else{
+        alert("El campo nombre y el campo apellido deben tener mas de 2 caracteres");
+        return false;
+    }
+}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+//utilizado y brindado por el template de bootstrap para que pueda ser aplicado en mas de 1 formulario
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 
+  //alert creado para el boton registrar en crear cuenta
+  function mostrarMensaje(){
+    alert("Operacion Exitosa");
+    }
+    
