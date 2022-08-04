751b1418ca733e755d296f6cdcb03cf53c31cc2b
ramiro <ramiro.g.g.1993@gmail.com>
2022-08-03 21:22:34 -0300

    Se agregan los primeros pasos de ecommerce

--- ecoomerce-Angular/src/app/app-routing.module.ts
+++ ecoomerce-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaProductoComponent } from './lista-producto/lista-producto.component';
+import { ListaCarritoComponent } from './lista-carrito/lista-carrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:ListaProductoComponent},
+  {path: 'carrito', component:ListaCarritoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
